ed15268a6cb7efa9b2ac641cdc3bbc307a4a6990
Arif <dewisandrax@gmail.com>
2019-09-29 21:42:45 +0700

    Update settings.py

--- twitter_django/settings.py
+++ twitter_django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=kaq^q@zpqc-cz63bt+7^x!_b%^vf_*qhehgxq%sng^9iu1@v2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
